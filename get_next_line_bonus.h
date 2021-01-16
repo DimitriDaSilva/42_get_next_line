d2fb40551865117ce9c41d7f4561ee132019d907
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-01-16 16:19:30 +0000

    Fixed format issues

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,27 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 21:52:42 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/01/16 16:04:04 by dda-silv         ###   ########.fr       */
+/*   Updated: 2021/01/16 16:17:55 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-// To delete
-# include <stdio.h>
-# include <fcntl.h>
-
-// To keep
 # include <stdlib.h>
 # include <unistd.h>
 
-# define MAX_LINE 8192 
+# define MAX_LINE 8192
 
 int		get_next_line(int fd, char **line);
 int		is_fd_valid(int fd);
